4f7d4925af13826c2c16f2d3678221426840af9a
rgerlyts <r.gerlyts@student.csn.khai.edu>
2018-06-26 17:33:36 +0300

    second lab

---
+++ Lab4/Fmas.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Lab4{
+    public class Fmas { 
+
+        public int[,] FA;
+        private int N;
+        XmlSerializer xmlSer = new XmlSerializer(typeof(int[]));
+
+        public Fmas(int n, int a, int b){
+            if (a > b){
+                int c = a;
+                a = b;
+                b = c;
+            }
+            N = n;
+            Random r = new Random();
+            FA = new int[n, n];
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    FA[i,j] = 0;
+                }
+            }
+            for (int i = 0; i < n; i++){
+                for (int j = 0; j < n; j++){
+                    FA[i,j] = r.Next(a, b + 1);
+                }
+            }
+        }
+
+        public bool CheckIndex(int i, int j) {
+            if(i > 0 && i < N && j > 0 && j < N)
+                return true;
+            return false;
+        }
+
+        public int[,] Func1(){
+            int [,] res = new int[N,N];
+            int [] tmp = new int[N];
+            for (int i = 0; i < N; i++){
+                for(int j = 0; j < N; j++)
+                    tmp[j] = FA[j, i];
+                Array.Sort(tmp);
+                Array.Reverse(tmp);
+                for(int j = 0; j < N; j++)
+                    res[j, i] = tmp[j];
+            }
+            return res;
+        }
+
+        public int[,] Func2(int d) {
+            int count = 0;
+            for(int i = 0; i < N; i++)
+                for(int j = 0; j < N; j++)
+                    if(FA[i,j] == d)
+                        count++;
+            int[,] res = new int[count, 2];
+            count = 0;
+            for(int i = 0; i < N; i++)
+                for(int j = 0; j < N; j++)
+                    if(FA[i,j] == d) {
+                        res[count, 0] = i;
+                        res[count, 1] = j;
+                        count++;
+                    }
+            return res;
+        }
+
+        public int[,] Func3(int h, int f) {
+            int[,] res = new int[N,N];
+            for(int i = 0; i < N; i++)
+                for(int j = 0; j < N; j++)
+                    if(FA[i,j] % 3 == 0)
+                        res[i,j] = FA[i,j] * (-1 * h);
+                    else
+                        res[i,j] = FA[i,j] - f;
+            return res;
+        }
+
+        public void Serialize(string fileName) {
+            int[] arr = new int[N * N];
+            int k = 0;
+            for(int i = 0; i < N; i++)
+                for(int j = 0; j < N; j++, k++)
+                    arr[k] = FA[i, j];
+            using(FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate)) {
+                xmlSer.Serialize(fs, arr);
+            }
+        }
+
+        public void Deserialize(string fileName) {
+            int[] arr = new int[N * N];
+            using(FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate)) {
+                arr = (int[])xmlSer.Deserialize(fs);
+            }
+            int k = 0;
+            for(int i = 0; i < N; i++)
+                for(int j = 0; j < N; j++, k++)
+                    FA[i, j] = arr[k];
+        }
+
+    }
+}
---
+++ Lab4/Form2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4 {
+    public partial class Form2 : Form {
+
+        public string ArrayParams = "";
+        public int[,] Source;
+        public int[,] Result;
+        public int N;
+
+        public Form2() {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e) {
+            textBox1.Text = ArrayParams;
+
+            dataGridView1.Columns.Clear();
+            dataGridView1.ColumnCount = N;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCellsExceptHeader;
+            for(int i = 0; i < N; i++) {
+                string[] temp = new string[N];
+                for(int j = 0; j < N; j++)
+                    temp[j] = Source[i,j].ToString();
+                dataGridView1.Rows.Add(temp);
+            }
+            dataGridView1.Refresh();
+
+            dataGridView2.Columns.Clear();
+            dataGridView2.ColumnCount = N;
+            dataGridView2.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+            dataGridView2.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCellsExceptHeader;
+            for(int i = 0; i < N; i++) {
+                string[] temp = new string[N];
+                for(int j = 0; j < N; j++)
+                    try {
+                        temp[j] = Result[i, j].ToString();
+                    } catch {
+                        temp[j] = " ";
+                    }
+                dataGridView2.Rows.Add(temp);
+            }
+        }
+    }
+}
