e188cea0312fa4a4e95e31b6423d18cd10fe1a28
Nikita Brihuntsov <n.brihuntsov@student.csn.khai.edu>
2018-06-26 17:18:57 +0300

    laba1

---
+++ laba1/laba_01_Calculator/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using laba_01_Calculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        Form1 x = new Form1();
+
+        [TestMethod]
+        //Тестирование суммы
+        public void TestMethodSum()
+        {
+            x.a = 10;
+            x.t = 6;
+            x.c = '+';
+            x.Calc();
+
+            if(x.b != 16)
+            {
+                Assert.Fail();
+            }
+
+        }
+        [TestMethod]
+        //Тестирование разности
+        public void TestMethodMinus()
+        {
+            x.a = 10;
+            x.t = 6;
+            x.c = '-';
+            x.Calc();
+
+            if (x.b != 4)
+            {
+                Assert.Fail();
+            }
+
+        }
+        [TestMethod]
+        //Тестирование умножения
+        public void TestMethodMult()
+        {
+            x.a = 10;
+            x.t = 6;
+            x.c = '*';
+            x.Calc();
+
+            if (x.b != 60)
+            {
+                Assert.Fail();
+            }
+
+        }
+        
+        [TestMethod]
+        //Тестирование деления
+        public void TestMethodDiv()
+        {
+            x.a = 60;
+            x.t = 6;
+            x.c = '/';
+            x.Calc();
+            if (x.b != 10)
+            {
+                Assert.Fail();
+            }
+        }
+        [TestMethod]
+        //Тестирование квадратного корня
+        public void TestMethodSqrt()
+        {
+            x.t = 4;
+            x.c = '^';
+            x.Calc();
+            if (x.b != 2)
+            {
+                Assert.Fail();
+            }
+        }
+        //Тестирование косинуса
+        [TestMethod]
+        public void TestMethodCosinus()
+        {
+            x.t = 0.5;
+            x.c = 'c';
+            x.Calc();
+            if (x.b != 0.877)
+            {
+                Assert.Fail();
+            }
+        }
+        [TestMethod]
+        //Тестирование факториала
+        public void TestMethodFactor()
+        {
+            x.a = 3;
+            x.c = '!';
+            x.Calc();
+            if (x.b != 6)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
---
+++ laba2/laba_2/UnitTestProject1/laba_2Test.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using laba_2;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace laba_2.Tests
+{
+    [TestClass]
+    public class laba_2Test
+    {
+        
+        equation exp = new equation();
+        // коэффициент а равен нулю
+        [TestMethod]
+        public void null_A()
+        {
+            exp.a = 0;
+            exp.b = 2;
+            exp.c = 3;
+
+            exp.calculate();
+            if(exp.x1 != -1.5)
+            {
+                Assert.Fail();
+            }
+        }
+        // коэффициент b равен нулю
+        [TestMethod]
+        public void null_B()
+        {
+            exp.a = 2;
+            exp.b = 0;
+            exp.c = -8;
+
+            exp.calculate();
+            if (exp.x1 != 2 && exp.x2 != -2)
+            {
+                Assert.Fail();
+            }
+        }
+        // коэффициент c равен нулю
+        [TestMethod]
+        public void null_C()
+        {
+            exp.a = 2;
+            exp.b = 3;
+            exp.c = 0;
+
+            exp.calculate();
+            if (exp.x1 != -1.5)
+            {
+                Assert.Fail();
+            }
+        }
+        //положительный дискриминант
+        [TestMethod]
+        public void positive_D()
+        {
+            exp.a = 1;
+            exp.b = 3;
+            exp.c = 2;
+
+            exp.calculate();
+            if (exp.D <= 0 && exp.x1 != -1 && exp.x2 != -2)
+            {
+                Assert.Fail();
+            }
+        }
+        //отрицательный дискриминант
+        [TestMethod]
+        public void negative_D()
+        {
+            exp.a = 1;
+            exp.b = 3;
+            exp.c = 4;
+
+            exp.calculate();
+            if (exp.D >= 0)
+            {
+                Assert.Fail();
+            }
+        }
+        //дискриминант равен нулю
+        [TestMethod]
+        public void equalNull_D()
+        {
+            exp.a = 2;
+            exp.b = 4;
+            exp.c = 2;
+
+            exp.calculate();
+            if (exp.D != 0 && exp.x1 != 1 && exp.x2 != 1)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
